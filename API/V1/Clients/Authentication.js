fa4b95499169adcdf0a467cac38d964f1a3d6ece
mmAbdelhay <abdelhay.courses@gmail.com>
2021-05-26 20:30:58 +0200

    fix regex of validating mobile phone

--- API/V1/Clients/Authentication.js
+++ API/V1/Clients/Authentication.js
@@ -4,26 +4,27 @@ const clientRepository = require("../../Repositories/ClientRepository");
 const tokenFunctions = require("../GlobalFunction/TokenFunctions");
 
 router.post("/register", async (req, res) => {
-   const validation_errors = register_info_validator(req.body);
-   if (validation_errors.length === 0) {
-      (await clientRepository.InsertClient(req.body))
-         ? res.status(200).json({ Message: "Registered Successfully" })
-         : res.status(500).json({ Message: "Database Error Occurred" });
-   } else {
-      res.status(400).json({ Message: validation_errors });
-   }
+  const validation_errors = register_info_validator(req.body);
+  console.log(validation_errors);
+  if (validation_errors.length === 0) {
+    (await clientRepository.InsertClient(req.body))
+      ? res.status(200).json({ Message: "Registered Successfully" })
+      : res.status(500).json({ Message: "Database Error Occurred" });
+  } else {
+    res.status(400).json({ Message: validation_errors });
+  }
 });
 
 router.post("/login", async (req, res) => {
-   const client_Found = await clientRepository.FindByEmail(req.body);
-   if (client_Found) {
-      res.status(200).json({
-         Message: "Client Logged in successfully",
-         token: tokenFunctions.generateToken(client_Found.id, "Client"),
-      });
-   } else {
-      res.status(400).json({ Message: "Client Credentials error" });
-   }
+  const client_Found = await clientRepository.FindByEmail(req.body);
+  if (client_Found) {
+    res.status(200).json({
+      Message: "Client Logged in successfully",
+      token: tokenFunctions.generateToken(client_Found.id, "Client"),
+    });
+  } else {
+    res.status(400).json({ Message: "Client Credentials error" });
+  }
 });
 
 module.exports = router;
--- API/V1/GlobalFunction/Validators/Validators.js
+++ API/V1/GlobalFunction/Validators/Validators.js
@@ -4,98 +4,104 @@ const states = require("../../../../assets/Config");
  * @param  {} name_to_validate
  */
 module.exports.validateName = (name_to_validate) => {
-   return /^[a-zA-Z ]*$/.test(name_to_validate) ? true : false;
+  return /^[a-zA-Z ]*$/.test(name_to_validate) ? true : false;
 };
 
 /**
  * @param  {} email_to_validate
  */
 module.exports.validateEmail = (email_to_validate) => {
-   return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-      email_to_validate
-   )
-      ? true
-      : false;
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+    email_to_validate
+  )
+    ? true
+    : false;
 };
 
 /**
  * @param  {} password_to_validate
  */
 module.exports.validatePassword = (password_to_validate) => {
-   return password_to_validate.length > 5 ? true : false;
+  return password_to_validate.length > 5 ? true : false;
 };
 
 /**
  * @param  {} gender_to_validate
  */
 module.exports.validateGender = (gender_to_validate) => {
-   return (gender_to_validate == "Male") | (gender_to_validate == "Female") ? true : false;
+  return (gender_to_validate == "Male") | (gender_to_validate == "Female")
+    ? true
+    : false;
 };
 
 /**
  * @param  {} country_to_validate
  */
 module.exports.validateCountry = (country_to_validate) => {
-   return countries_list.some((object) => object.name === country_to_validate);
+  return countries_list.some((object) => object.name === country_to_validate);
 };
 /**
  * @param  {} mobile_to_validate
  */
 
 module.exports.validateMobile = (mobile_to_validate) => {
-   return /^(\+\d{1,2})?\d{10}$/.test(mobile_to_validate) ? true : false;
+  return /^(\+\d{1,4})\d{10,12}$/.test(mobile_to_validate) ? true : false;
 };
 
 /**
  * @param  {} date_of_birth_to_validate
  */
 module.exports.validateDateOfBirth = (date_of_birth_to_validate) => {
-   return /^((19|20)\d{2})\-(0[1-9]|1[0-2])\-(0[1-9]|1\d|2\d|3[01])$/.test(
-      date_of_birth_to_validate
-   )
-      ? true
-      : false;
+  return /^((19|20)\d{2})\-(0[1-9]|1[0-2])\-(0[1-9]|1\d|2\d|3[01])$/.test(
+    date_of_birth_to_validate
+  )
+    ? true
+    : false;
 };
 /**
  * @param  {} coord_to_validate
  */
 module.exports.validateCoordinates = (coord_to_validate) => {
-   return isFinite(coord_to_validate) && Math.abs(coord_to_validate) <= 90 ? true : false;
+  return isFinite(coord_to_validate) && Math.abs(coord_to_validate) <= 90
+    ? true
+    : false;
 };
 /**
  * @param  {} provider_type_to_validate
  */
 module.exports.validateProvider = (provider_type_to_validate) => {
-   return states.Types.includes(provider_type_to_validate) ? true : false;
+  return states.Types.includes(provider_type_to_validate) ? true : false;
 };
 /**
  * @param  {} coverage_zone_to_validate
  */
 module.exports.validateCoverageZone = (coverage_zone_to_validate) => {
-   return coverage_zone_to_validate > 0 ? true : false;
+  return coverage_zone_to_validate > 0 ? true : false;
 };
 /**
  * @param  {} time_to_validate
  */
 module.exports.validateTime = (time_to_validate) => {
-   return /^(2[0-3]|[0-1]?[\d]):[0-5][\d]:[0-5][\d]$/.test(time_to_validate) ? true : false;
+  return /^(2[0-3]|[0-1]?[\d]):[0-5][\d]:[0-5][\d]$/.test(time_to_validate)
+    ? true
+    : false;
 };
 /**
  * @param  {} delivery_fee_to_validate
  */
 module.exports.validateDeliveryFee = (delivery_fee_to_validate) => {
-   return delivery_fee_to_validate >= 0 ? true : false;
+  return delivery_fee_to_validate >= 0 ? true : false;
 };
 /**
  * @param  {} minimum_order_to_validate
  */
 module.exports.validateMinimumOrder = (minimum_order_to_validate) => {
-   return minimum_order_to_validate > 0 ? true : false;
+  return minimum_order_to_validate > 0 ? true : false;
 };
 /**
  * @param  {} openingTime
  * @param  {} ClosingTime
  */
 module.exports.validateOpeningClosingTime = (openingTime, ClosingTime) => {
-   return openingTime.slice(0, 2) < ClosingTime.slice(0, 2) ? true : false;
+  return openingTime.slice(0, 2) < ClosingTime.slice(0, 2) ? true : false;
 };
