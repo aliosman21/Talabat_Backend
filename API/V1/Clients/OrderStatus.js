ce706847d38a24d26e624b264cb2b487dea54a6a
aliosman21 <amrali975@gmail.com>
2021-06-25 14:51:44 +0200

    Changed server is up

--- API/V1/Clients/OrderStatus.js
+++ API/V1/Clients/OrderStatus.js
@@ -4,8 +4,6 @@ const jwt = require("jsonwebtoken");
 const VerifyClearance = require("../GlobalFunction/VerifyUsersClearance");
 
 router.get("/:id", VerifyClearance.CheckAccessPrivilege("Client"), async (req, res) => {
-   // console.log("in right path");
-   // console.log(req.params.id);
    const orderStatus = await OrderRepo.FindOrderStatus(req.params.id);
    if (orderStatus) {
       res.status(200).json({ orderStatus });
